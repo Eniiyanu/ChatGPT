6b0eb4ad7280740f698b74885eae285ec87908de
eniiyanu <depopf89@gmail.com>
2023-03-12 00:05:32 +0100

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import {BrowserRouter as Router, Switch, Route, BrowserRouter} from "react-router-dom"
+import {BrowserRouter as Router, Switch, Routes, Route, BrowserRouter} from "react-router-dom"
 
 
 function App() {
