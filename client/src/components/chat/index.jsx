2882abb653f04488377fe7288032bb40e03b7f4e
eniiyanu <depopf89@gmail.com>
2023-03-12 00:19:33 +0100

    update client/src/components/chat/index.jsx

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced'
 const Chat = () => {
-  return (
-    <div>index</div>
-  )
+
+    const { chatConfig, chatState, chatActions } = useMultiChatLogic(
+        import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID,
+    )
+  return <div>Chat</div>;
+  
 }
 
 export default Chat
