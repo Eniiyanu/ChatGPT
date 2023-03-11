15dea2bcabb4a23ebc915b865086062e1f42b3fe
eniiyanu <depopf89@gmail.com>
2023-03-12 00:22:02 +0100

    update client/src/components/chat/index.jsx

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced'
 const Chat = () => {
 
-    const { chatConfig, chatState, chatActions } = useMultiChatLogic(
+    const chatProps{ chatConfig, chatState, chatActions } = useMultiChatLogic(
         import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID,
         "Enii",
         "test"
     )
   return <div style={{flexBasis: "100%"}}>
-  <MultiChatSocket
+  <MultiChatSocket {...chatProps} />
+    <MultiChatWindow {...chatProps} />
   </div>;
   
 }
