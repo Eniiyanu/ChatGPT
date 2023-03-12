87530a6e4d6a182145524f21ca168cb340c8c65c
eniiyanu <depopf89@gmail.com>
2023-03-12 23:55:14 +0100

    update StandardMessageForm.jsx

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import React, {useState} from "react";
 
 const StandardMessageForm = () => {
   const [message, setMessage, sendMessage, activeChat] = useState("");
   const [attachment, setAttachment] = useState("");
   const [preview, setPreview] = useState("");
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
   return (
-    <div className='message-form-container'>
-
-{preview && (
-  <div className='message-form-preview'> 
-    <img className='message-form-preview-image' src={preview} onLoad={() => URL.revokeObjectURL(preview)} alt='preview' />
-  </div>
-  )}
+    <div className="message-form-container">
+      {preview && (
+        <div className="message-form-preview">
+          <img
+            className="message-form-preview-image"
+            src={preview}
+            onLoad={() => URL.revokeObjectURL(preview)}
+            alt="preview"
+          />
+          <XMarkIcon
+            className="messsage-form-preview-x"
+            onClick={() => {
+              setPreview("");
+              setAttachment("");
+            }}
+          />
+        </div>
+      )}
+      <div className="message-form">
+        <div>
+          <imput
+            className="message-form-input"
+            type="text"
+            value={message}
+            onchange={handleChange}
+            placeholder="Type a message..."
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StandardMessageForm
+export default StandardMessageForm;
