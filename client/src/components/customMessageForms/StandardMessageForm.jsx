35d009c09fa0f85a80bdd29ac0c134166ae0794a
eniiyanu <depopf89@gmail.com>
2023-03-12 23:47:08 +0100

    update StandardMessageForm.jsx

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -5,9 +5,13 @@ const StandardMessageForm = () => {
   const [attachment, setAttachment] = useState("");
   const [preview, setPreview] = useState("");
   return (
-    <div>
+    <div className='message-form-container'>
 
-      
+{preview && (
+  <div className='message-form-preview'> 
+    <img className='message-form-preview-image' src={preview} onLoad={() => URL.revokeObjectURL(preview)} alt='preview' />
+  </div>
+  }
     </div>
   )
 }
